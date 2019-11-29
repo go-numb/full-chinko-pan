2e291300a32abb32274fdb96403fa6bead823750
numbp <o9o73286699@gmail.com>
2019-11-29 21:47:37 +0900

    mean exec times

--- replace/string_test.go
+++ replace/string_test.go
@@ -4,29 +4,42 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"gonum.org/v1/gonum/stat"
 )
 
 func TestString(t *testing.T) {
-	start := time.Now()
-	defer func() {
-		end := time.Now()
-		fmt.Println("exec time: ", end.Sub(start))
-	}()
-
-	base := "あああああああああああああああああああああああああああああああ！！！！！！！！！！！(ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌ"
+	base := "ファルコン・パンチ"
+	words := len([]rune(base))
 
 	var (
 		count int
 		name  string
+
+		meanCount = 100
+
+		// 平均回数、平均時間を取得
+		counts = make([]float64, meanCount)
+		times  = make([]float64, meanCount)
 	)
-	for {
-		name = NameOfToday(base)
-		if name == base {
-			break
+
+	for i := 0; i < meanCount; i++ {
+		count = 0
+		start := time.Now()
+
+		for {
+			name = NameOfToday(base)
+			if name == base {
+				break
+			}
+			count++
 		}
-		count++
-		time.Sleep(time.Millisecond)
+
+		fmt.Printf("%d times success\n", i)
+		counts[i] = float64(count)
+		times[i] = time.Now().Sub(start).Seconds()
 	}
 
-	fmt.Printf("%d: %s\n", count, name)
+	fmt.Println(base)
+	fmt.Printf("culc count: %d, words: %d, words, Count: %.3ftimes/1word, Time: %.3fsec/1word\n", meanCount, words, stat.Mean(counts, nil), stat.Mean(times, nil))
 }
